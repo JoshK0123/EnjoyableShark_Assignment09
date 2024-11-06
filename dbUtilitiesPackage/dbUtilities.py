92cf9d41554497fbeb477ebc8324d6bdd3cc04d2
Joshua Klingelhafer <klingejh@mail.uc.edu>
2024-11-05 19:25:16 -0500

    Commit to add documentation.

--- dbUtilitiesPackage/dbUtilities.py
+++ dbUtilitiesPackage/dbUtilities.py
@@ -1,5 +1,16 @@
 #dbUtilities.py
 
+# Name: Joshua Klingelhafer, Lucas Ransick, Ryan Dew
+# email: klingejh@mail.uc.edu, ransiclg@mail.uc.edu, dewrm@mail.uc.edu
+# Assignment Number: Assignment 09
+# Due Date:  11/7
+# Course #/Section:   IS4010 001
+# Semester/Year:   Fall 2024
+# Brief Description of the assignment:  Connect to a database and create SQL queries to learn about grocery items from a database.
+# Brief Description of what this module does: This module connects us to the SQL database.
+# Citations: Stack Overflow
+# Anything else that's relevant:
+
 import pyodbc
 class DatabaseConnection(object):
     """
--- mainPackage/main.py
+++ mainPackage/main.py
@@ -1,50 +1,15 @@
 # main.py
 
-import random
-import pyodbc
-from dbUtilitiesPackage.dbUtilities import *
-
-if __name__ == "__main__":
-    """
-    This package attempts to connect to the DB and uses queries and its data to make a sentence of it
-    """
-    try:
-        cursor = DatabaseConnection.connect_to_database()
-    
-    except Exception as e:
-        print("Error accessing database")
-        print(e)
-        exit()
-        
-    emptyListForData = []
-    query_string = "SELECT ProductID, [UPC-A ], Description, ManufacturerID, BrandID  FROM tProduct"
-    results = cursor.execute(query_string)
-    for row in results.fetchall(): 
-        emptyListForData.append(row)
-    random_row_from_list = random.choice(emptyListForData)
-    
-    productID = random_row_from_list[0]
-    description = random_row_from_list[2]
-    manufacturerID = random_row_from_list[3]
-    brandID = random_row_from_list[4]
-    
-    query_string_manufacturer = "SELECT Manufacturer FROM tManufacturer WHERE ManufacturerID = " + str(manufacturerID)
-    Manufacturer = cursor.execute(query_string)
-    for row in results.fetchone(): 
-        Manufacturer = row
-
-    query_string_brand = "SELECT Brand FROM tBrand WHERE BrandID = " + str(brandID)
-    Brand = cursor.execute(query_string)
-    for row in results.fetchone(): 
-        Brand = row
-
-
-    number_sold_query_string = "SELECT TOP (100) PERCENT SUM(dbo.tTransactionDetail.QtyOfProduct) AS NumberOfItemsSold FROM dbo.tTransactionDetail INNER JOIN dbo.tTransaction ON dbo.tTransactionDetail.TransactionID = dbo.tTransaction.TransactionID WHERE (dbo.tTransaction.TransactionTypeID = 1) AND (dbo.tTransactionDetail.ProductID = " + str(productID) + ") "
-    number_sold = cursor.execute(number_sold_query_string)
-    for row in number_sold.fetchone(): 
-        number_sold = row
-   
-# main.py
+# Name: Joshua Klingelhafer, Lucas Ransick, Ryan Dew
+# email: klingejh@mail.uc.edu, ransiclg@mail.uc.edu, dewrm@mail.uc.edu
+# Assignment Number: Assignment 09
+# Due Date:  11/7
+# Course #/Section:   IS4010 001
+# Semester/Year:   Fall 2024
+# Brief Description of the assignment: Connect to a database and create SQL queries to learn about grocery items from a database.
+# Brief Description of what this module does: This module runs the SQL queries and stores them into variables. It then prints a statement that explains product details, according to the data that is taken from the database.
+# Citations: Stack Overflow
+# Anything else that's relevant:
 
 import random
 import pyodbc
