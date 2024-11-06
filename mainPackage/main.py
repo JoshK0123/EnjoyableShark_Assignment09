18476246b53c856f44d3afea7f3b3142dd0213c8
Joshua Klingelhafer <klingejh@mail.uc.edu>
2024-11-05 19:17:47 -0500

    Commit to add step 7, the sentence that explains the product details.

--- mainPackage/main.py
+++ mainPackage/main.py
@@ -44,3 +44,52 @@ if __name__ == "__main__":
     for row in number_sold.fetchone(): 
         number_sold = row
    
+# main.py
+
+import random
+import pyodbc
+from dbUtilitiesPackage.dbUtilities import *
+
+if __name__ == "__main__":
+    """
+    This package attempts to connect to the DB and uses queries and its data to make a sentence of it
+    """
+    try:
+        cursor = DatabaseConnection.connect_to_database()
+    
+    except Exception as e:
+        print("Error accessing database")
+        print(e)
+        exit()
+        
+    emptyListForData = []
+    query_string = "SELECT ProductID, [UPC-A ], Description, ManufacturerID, BrandID  FROM tProduct"
+    results = cursor.execute(query_string)
+    for row in results.fetchall(): 
+        emptyListForData.append(row)
+    random_row_from_list = random.choice(emptyListForData)
+    
+    productID = random_row_from_list[0]
+    description = random_row_from_list[2]
+    manufacturerID = random_row_from_list[3]
+    brandID = random_row_from_list[4]     
+
+    manufacturer_query_string = "SELECT Manufacturer FROM tManufacturer WHERE ManufacturerID = " + str(manufacturerID)
+    Manufacturer = cursor.execute(manufacturer_query_string)
+    for row in Manufacturer.fetchone():
+         Manufacturer = row
+    
+    brand_query_string = "SELECT Brand FROM tBrand WHERE BrandID = " + str(brandID)
+    Brand = cursor.execute(brand_query_string)
+    for row in Brand.fetchone():
+        Brand = row
+
+
+    number_sold_query_string = "SELECT TOP (100) PERCENT SUM(dbo.tTransactionDetail.QtyOfProduct) AS NumberOfItemsSold FROM dbo.tTransactionDetail INNER JOIN dbo.tTransaction ON dbo.tTransactionDetail.TransactionID = dbo.tTransaction.TransactionID WHERE (dbo.tTransaction.TransactionTypeID = 1) AND (dbo.tTransactionDetail.ProductID = " + str(productID) +")"
+    number_sold = cursor.execute(number_sold_query_string)
+    for row in number_sold.fetchone():
+        number_sold = row
+
+    Sentence = "The product " + str(Brand) + " is manufactured by " + str(Manufacturer) + ", has a description of " + str(description) + " and at this grocery store location " + str(number_sold) + " have sold."
+    print(Sentence)
+
